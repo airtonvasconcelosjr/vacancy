5540bad4b71c21f042740b22f53e6734696667f0
airtonvasconcelosjr <airton.vasconcelosjr@gmail.com>
2023-07-10 14:20:05 -0300

    Front end test - Vagas.com

--- index.js
+++ index.js
@@ -1,52 +1,67 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const qtdInput = document.getElementById('qtdInput');
-    const plusBtn = document.getElementById('plusBtn');
-    const minusBtn = document.getElementById('minusBtn');
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    const checkboxErrorMessage = document.getElementById('checkboxErrorMessage');
-  
-    plusBtn.addEventListener('click', (event) => {
-      event.preventDefault();
-      qtdInput.stepUp(); 
-    });
-  
-    minusBtn.addEventListener('click', (event) => {
-      event.preventDefault();
-      qtdInput.stepDown(); 
-    });
-  
-    const form = document.querySelector('form');
-    const quantityInput = document.getElementById('qtdInput');
-  
-    form.addEventListener('submit', function(event) {
-      if (quantityInput.value === '0') {
-        quantityInput.classList.add('error');
-      } else {
-        console.log(quantityInput.value);
-        quantityInput.classList.remove('error');
-      }
-  
-      let checkedCount = 0;
-      checkboxes.forEach(function(checkbox) {
-        if (checkbox.checked) {
-          checkedCount++;
-          event.preventDefault();
-          console.log(checkbox.value); 
-        }
-      });
-  
-      if (checkedCount === 0) {
-        event.preventDefault();
-        if (checkboxErrorMessage) {
-          checkboxErrorMessage.innerText = 'Selecione pelo menos um sticker';
-          checkboxErrorMessage.style.display = 'block';
-          sucesso.style.display = 'block';
-        }
-      } else {
-        if (checkboxErrorMessage) {
-          checkboxErrorMessage.style.display = 'none';
-        }
-      }
-    });
+  const qtdInput = document.getElementById('qtdInput');
+  const plusBtn = document.getElementById('plusBtn');
+  const minusBtn = document.getElementById('minusBtn');
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  const checkboxErrorMessage = document.getElementById('checkboxErrorMessage');
+  const form = document.querySelector('form');
+  const descricao = document.getElementById('descricao');
+  const sucesso = document.getElementById('sucesso');
+
+  plusBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    qtdInput.stepUp(); 
   });
+
+  minusBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    qtdInput.stepDown(); 
+  });
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let checkedCount = 0;
+    checkboxes.forEach(function(checkbox) {
+      if (checkbox.checked) {
+        checkedCount++;
+      }
+    });
+
+    if (checkedCount === 0 || qtdInput.value <= 0) {
+      if (checkedCount === 0 && checkboxErrorMessage) {
+        checkboxErrorMessage.innerText = 'Selecione pelo menos um sticker';
+        checkboxErrorMessage.style.display = 'block';
+        setTimeout(function() {
+          checkboxErrorMessage.style.display = 'none';
+        }, 3000);
+      };
+
+      if (qtdInput.value <= 0) {
+           qtdInput.classList.add('error');
+           setTimeout(function() {
+            qtdInput.classList.remove('error');
+          }, 3000);
+      }
+
+      return; 
+    }
+
+    console.log('Stickers selecionados:');
+    checkboxes.forEach(function(checkbox) {
+      if (checkbox.checked) {
+        console.log(checkbox.value);
+      }
+    });
+
+    console.log('Quantidade:', qtdInput.value);
+    console.log('Descrição:', descricao.value);
+
+    sucesso.style.display = 'block';
+    setTimeout(function() {
+      sucesso.style.display = 'none';
+    }, 3000);
   
+    form.reset(); 
+  });
+});
